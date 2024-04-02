f68673eb777769dd76887c32f3c959c9b5fa7724
Letícia Zalasik <leticiazalasik@gmail.com>
2024-04-02 14:58:15 -0300

    :construction: Feat: (test) Criaçao de uma JOptionPane de teste

---
+++ src/br/com/projetoMvc/test/ExemploJOptionPane.java
@@ -0,0 +1,20 @@
+package br.com.projetoMvc.test;
+
+import javax.swing.JOptionPane;
+
+public class ExemploJOptionPane {
+	
+	public static void main(String[] args) {
+
+
+	String nome = JOptionPane.showInputDialog("Digite seu nome: "); 
+	JOptionPane.showMessageDialog(null, "Nome digitado: " + nome); 
+	
+	String menu = "Menu de opções" + 
+	"\n[1] Cadastrar" +
+	"\n[2] Listar Todos"; 
+	
+	int opcao = Integer.parseInt(JOptionPane.showInputDialog(menu)); 
+	JOptionPane.showMessageDialog(null, opcao); 
+}
+}
