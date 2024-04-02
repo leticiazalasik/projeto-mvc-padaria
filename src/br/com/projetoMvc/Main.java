57daea97aa94672d1763d8ee802d3e83bb00476e
Letícia Zalasik <leticiazalasik@gmail.com>
2024-04-02 14:42:32 -0300

    :test_tube: (controller, main): Feat: Adicionei a classe Produto controller e um teste na classe main da conexão com o Banco de dados

--- src/br/com/projetoMvc/Main.java
+++ src/br/com/projetoMvc/Main.java
@@ -1,10 +1,29 @@
 package br.com.projetoMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.projetoMvc.controller.ProdutoController;
+import br.com.projetoMvc.model.Produto;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//testando o listar todos 
+		ProdutoController controller = new ProdutoController(); 
+		
+		System.out.println("- Lista de produtos -");
+		List <Produto>lista = new ArrayList<Produto>();
+		lista = controller.listarTodos(); 
+		
+		for (Produto produto : lista) {
+			System.out.print(produto.getId());
+			System.out.print("-");
+			System.out.println(produto.getDescricao());
+		}
+		
 	}
 
 }
