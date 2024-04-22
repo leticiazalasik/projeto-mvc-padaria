5d0a17e0ec1624a482f735b5a4a15d09175409c7
Letícia Zalasik <leticiazalasik@gmail.com>
2024-04-22 13:27:09 -0300

    :construction: Feat (main): Criando um menu com JOptionPane

--- src/br/com/projetoMvc/Main.java
+++ src/br/com/projetoMvc/Main.java
@@ -15,6 +15,7 @@ public class Main {
 		
 		//Criando Menu 
 		
+		
 		String menu = " "
 				.concat(" ** Menu do sistema **\n")
 				.concat(" 1- Cadastrar \n")
@@ -25,13 +26,15 @@ public class Main {
 				.concat(" 6- Finalizar \n")
 				.concat(" Digite a opção desejada: \n"); 
 		
-		String option = JOptionPane.showInputDialog(menu); 
-//		int optionMenu = Integer.parseInt(option); 
-		
-		
-		
-		//testando o listar todos 
+		int option = Integer.parseInt(JOptionPane.showInputDialog(menu));
 		DoceController controller = new DoceController(); 
+
+		while (option!=6) { 
+
+		switch (option) { 
+		
+		case 2: 
+		//testando o listar todos 
 		
 		System.out.println("- Lista de produtos -");
 		List <Doce>lista = new ArrayList<Doce>();
@@ -49,7 +52,12 @@ public class Main {
 			
 		}
 		
+		option = Integer.parseInt(JOptionPane.showInputDialog(menu));
+
+		break; 
 		
+		
+		case 3: 
 		int opcao = Integer.parseInt(JOptionPane.showInputDialog("Digite o id: ")); 
 		
 		Doce 	doceEncontrado = controller.listarPorId(opcao);
@@ -79,17 +87,27 @@ public class Main {
 			JOptionPane.showMessageDialog(null, "Não existe produto com esse código na lista");
 		}
 		
-////		//Cadastrar novo doce 
-//		JOptionPane.showMessageDialog(null, "*****\n Cadastro de produto novo: "); 
-//		Doce doce1 = new Doce(); 
-//		doce1.setNome(JOptionPane.showInputDialog("Digite o nome: "));
-//		doce1.setCustoProduto(Double.parseDouble(JOptionPane.showInputDialog("Digite o custo do produto: ")));
-//		doce1.setIsAtivo(Boolean.parseBoolean(JOptionPane.showInputDialog("Digite se o produto está ativo: ")));
-//		doce1.setMargemLucro(Double.parseDouble(JOptionPane.showInputDialog("Digite a porcentagem de lucro: ")));
-//		doce1.setOpcaoTamanho(JOptionPane.showInputDialog("Inteiro/Pedaço: "));
+		option = Integer.parseInt(JOptionPane.showInputDialog(menu));
 
-//		controller.cadastrar(doce1); 
+		break; 
 		
+		case 1: 
+//		//Cadastrar novo doce 
+		JOptionPane.showMessageDialog(null, "*****\n Cadastro de produto novo: "); 
+		Doce doce1 = new Doce(); 
+		doce1.setNome(JOptionPane.showInputDialog("Digite o nome: "));
+		doce1.setCustoProduto(Double.parseDouble(JOptionPane.showInputDialog("Digite o custo do produto: ")));
+		doce1.setIsAtivo(Boolean.parseBoolean(JOptionPane.showInputDialog("Digite se o produto está ativo: ")));
+		doce1.setMargemLucro(Double.parseDouble(JOptionPane.showInputDialog("Digite a porcentagem de lucro: ")));
+		doce1.setOpcaoTamanho(JOptionPane.showInputDialog("Inteiro/Pedaço: "));
+
+		controller.cadastrar(doce1); 
+		
+		option = Integer.parseInt(JOptionPane.showInputDialog(menu));
+
+		break; 
+		
+		case 5: 
 //testando o excluir 
 	
 	int op = Integer.parseInt(JOptionPane.showInputDialog("Digite o id a ser excluído: "));
@@ -101,6 +119,27 @@ public class Main {
 		JOptionPane.showMessageDialog(null, "Erro ao excluir produto " + op); 
 	}
 		
+	option = Integer.parseInt(JOptionPane.showInputDialog(menu));
+
+		break; 
+		
+		case 4: 
+			System.out.println("Editar ainda preciso fazer");
+			
+		case 6: 
+			JOptionPane.showMessageDialog(null, "Sistema finalizado!"); 
+			
+			
+			break; 
+			
+			default: 
+				JOptionPane.showMessageDialog(null, "Essa opçao nãso existe, escolha novamente."); 
+				option = Integer.parseInt(JOptionPane.showInputDialog(menu));
+
+	
 	}
 
+		}
+		
+	}
 }
